9cf36ac8d07f548629e3cba6373c26e15de74e9c
Federico Minaya <elfedeminaya@gmail.com>
2019-07-14 21:23:36 -0300

    cerillas agreggated

---
+++ cerillas.py
@@ -0,0 +1,15 @@
+import math
+
+T = int(input(""))
+
+casos = []
+
+for i in range(T):
+  e = int(input(""))
+  casos.append(e)
+
+for caso in casos:
+  resultado = 0
+  for i in range(caso + 1):
+    resultado += 3 * i
+  print(resultado)
